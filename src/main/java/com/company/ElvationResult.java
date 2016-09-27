b0cd2080be2af3e094cc45545532ba216fee0fc3
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-09-27 12:32:28 -0500

    First GoogleMaps API

---
+++ src/main/java/com/company/ElvationResult.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by ox6185ys on 9/27/2016.
+ */
+public class ElvationResult {
+    public String elevation;
+}
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
-
+import com.google.maps.ElevationApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.model.ElevationResult;
+import com.google.maps.model.LatLng;
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String key = null;
+        try(BufferedReader reader = new BufferedReader(new FileReader("key.txt"))){
+            key = reader.readLine();
+            System.out.println(key);
+        }catch (Exception ioe){
+            System.out.println("No key file found, or could not be read.");
+            System.exit(-1);}
+
+            GeoApiContext context = new GeoApiContext().setApiKey(key);
+            LatLng mctcLatLng = new LatLng(44.973074, -93.283356);
+
+        ElevationResult[] results = new ElevationResult[0];
+        try {
+            results = ElevationApi.getByPoints(context, mctcLatLng).await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (results.length >= 1){
+                ElevationResult mctcElevation = results[0];
+                System.out.println("The elevation of MCTC is "+mctcElevation.elevation+" meters.");
+                System.out.println(String.format("The elevation of MCTC above sea level is %.2f meters",mctcElevation.elevation));
+            }
+        }
     }
-}
+
