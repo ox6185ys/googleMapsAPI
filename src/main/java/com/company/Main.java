5dea4d3269b3a0f6231126a5b648c5b8a60beb77
Patrick <ox6185ys@go.minneapolis.edu>
2016-09-28 13:12:47 -0500

    Update gitignore

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,7 +6,7 @@ import com.google.maps.model.LatLng;
 import java.io.BufferedReader;
 import java.io.FileReader;
 public class Main {
-
+//week6 geocoding
     public static void main(String[] args) {
         String key = null;
         try(BufferedReader reader = new BufferedReader(new FileReader("key.txt"))){
@@ -16,8 +16,9 @@ public class Main {
             System.out.println("No key file found, or could not be read.");
             System.exit(-1);}
             GeoApiContext context = new GeoApiContext().setApiKey(key);
-//44.973074, -93.283356
-            LatLng mctcLatLng = new LatLng(44.973074, -93.283356);
+//44.973074, -93.283356 MCTC LatLng
+//37.7749, 122.4194 SF LatLng
+            LatLng mctcLatLng = new LatLng(37.7749, -122.4197);
         ElevationResult[] results = new ElevationResult[0];
         try {
             results = ElevationApi.getByPoints(context, mctcLatLng).await();
