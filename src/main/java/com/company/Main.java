a5cf276957dad4b54d1dadc259d0bef45c6a939c
ox6185ys <ox6185ys@go.minneapolis.edu>
2016-09-28 16:44:54 -0500

    Geocodingresult GoogleMaps

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,8 @@
 package com.company;
-
+import com.google.maps.ElevationApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApiRequest;
-
+import com.google.maps.model.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -10,11 +10,9 @@ import java.util.Scanner;
 public class Main {
     static Scanner stringScanner = new Scanner(System.in);
     static Scanner numberScanner = new Scanner(System.in);
-
 //week6 geocoding
-
-    //import buffread for key.txt
-    public static void main(String[] args) {
+//import Bufferedreader for key.txt
+    public static void main(String[] args) throws Exception{
         String key = null;
         try{
             BufferedReader reader = new BufferedReader(new FileReader("key.txt"));
@@ -25,35 +23,47 @@ public class Main {
             System.out.println("No key file found, or could not be read.");
             System.exit(-1);
         }
-
-
         GeoApiContext context = new GeoApiContext().setApiKey(key);
         GeocodingApiRequest locationRequest = new GeocodingApiRequest(context);
-        System.out.println("Enter a location to see LatLng.");
+        System.out.println("Enter a city to see its latitude and longitude.");
         String location = stringScanner.nextLine();
-        String baseURL = String.format("https://maps.googleapi.com/maps/api/geocode/json?address=%s&key=%s", location, key);
+        //String baseURL = String.format("https://maps.googleapi.com/maps/api/geocode/json?address=%s&key=%s", location, key);
 
-        GeocodingApiRequest jsonResult = locationRequest.address(baseURL);
+        GeocodingResult[] geocodingResultsArray = locationRequest.address(location).await();
+        //System.out.println(baseURL);
+        //System.out.println(geocodingResults[0]);
+        Bounds bounds = new Geometry().bounds;
+        System.out.println(locationRequest);
+
+        if (geocodingResultsArray.length > 0) {
+            GeocodingResult firstGeocodingResult = geocodingResultsArray[0];
+            LatLng latlong = firstGeocodingResult.geometry.location;//geometry and location are tags under "GeocodingResult".
+
+            System.out.println(latlong);
+
+            //todo - Use the latlong object to make an elevation request.
+            ElevationResult[] elevationResultsArray = ElevationApi.getByPoints(context, latlong).await();
+
+        } else {
+            System.out.println("No results found");
+            //todo ask user again, or whatever
+        }
 
 
-//        System.out.println(String.format("The elevation of MCTC above sea level is %.2f meters",mctcElevation.elevation));
 
 
 
 
+        //        System.out.println(String.format("The elevation of MCTC above sea level is %.2f meters",mctcElevation.elevation));
 /*        System.out.println("Enter a location to see LatLng.");
         String location = stringScanner.nextLine();*/
-
-
 /*
-
- import com.google.maps.ElevationApi;
+import com.google.maps.ElevationApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.ElevationResult;
 import com.google.maps.model.LatLng;
 import java.io.BufferedReader;
 import java.io.FileReader;
-
 String key = null;
         try(BufferedReader reader = new BufferedReader(new FileReader("key.txt"))){
             key = reader.readLine();
@@ -77,6 +87,4 @@ String key = null;
                 System.out.println(String.format("The elevation of MCTC above sea level is %.2f meters",mctcElevation.elevation));
            }*/
     }
-
-
 }
